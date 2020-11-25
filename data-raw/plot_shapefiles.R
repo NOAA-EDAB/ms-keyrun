0c6967318894504bd30181d2a4b9f575581afa15
andybeet <22455149+andybeet@users.noreply.github.com>
2020-11-25 13:01:14 -0500

    changed data_raw foldere to data-raw

--- data-raw/plot_shapefiles.R
+++ data-raw/plot_shapefiles.R
@@ -8,9 +8,9 @@ ggplot2::ggplot(data=EPU_strata) +
   ggplot2::geom_sf(ggplot2::aes(fill=Strata))
 
 # combines EPU and statistical area
-EPU_stat <- sf::st_read(here::here("gis","GB_EPU_stat.shp"))
+stat_Areas <- sf::st_read(here::here("gis","Statistical_Areas_2010.shp"))
 
-ggplot2::ggplot(data=EPU_stat) +
+ggplot2::ggplot(data=stat_Areas) +
   ggplot2::geom_sf(ggplot2::aes(fill=Id))
 
 # all survey strata
@@ -25,10 +25,14 @@ EPU10 <- sf::st_read(here::here("gis","EPU_extended.shp"))
 ggplot2::ggplot(data=EPU10) +
   ggplot2::geom_sf(ggplot2::aes(fill=EPU))
 
-# NEW GB strata  min square definitions
+# NEW GB EPU def based on survey strata
 GB_strata <- sf::st_read(here::here("gis","GB_SOE_strata.shp"))
 
 ggplot2::ggplot(data=GB_strata) +
   ggplot2::geom_sf(ggplot2::aes(fill=EPU))
 
+# GB strata min square definitions
+GB_EPU_stat <- sf::st_read(here::here("gis","GB_SOE_strata_stat.shp"))
 
+ggplot2::ggplot(data=GB_EPU_stat) +
+  ggplot2::geom_sf(ggplot2::aes(fill=Id))
