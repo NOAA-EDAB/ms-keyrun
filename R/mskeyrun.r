50c85ee5fd7b1333e23274bda683fe385686bca4
andybeet <22455149+andybeet@users.noreply.github.com>
2021-09-07 15:16:40 -0400

    add package documentation R script

---
+++ R/mskeyrun.r
@@ -0,0 +1,17 @@
+#' mskeyrun: 
+#'
+#' This Data package documents decisions made regarding datasets and modeling for multispecies model 
+#' keyruns for Georges Bank, USA. Data and methods documentation are found here.
+#'
+#'\itemize{
+#'\item Bundled data 
+#'\item Helper scrips for other file format (shp, csv)
+#'\item Documetation of all steps to generate data
+#'}
+#'
+#'To learn more about using \code{mskeyrun}, start with the vignette: \code{browseVignettes(package="mskerun")} or click the index below
+#'
+#'
+#' @docType package
+#' @name mskeyrun
+NULL
