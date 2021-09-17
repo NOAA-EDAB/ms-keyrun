de97d3b610f00eb3a27120922810c31fd871525c
Sarah Gaichas <sarah.gaichas@noaa.gov>
2021-09-17 18:48:44 -0400

    add documentation

---
+++ R/simFocalSpecies.R
@@ -0,0 +1,16 @@
+#' The Atlantis-simulated focal species in mskeyrun project
+#'
+#' Model name and scenario, Species name, latin name, and model species code
+#'
+#' @format A data frame
+#'
+#' \describe{
+#'\item{ModSim}{Atlantis model name and simulation id}
+#'\item{Code}{Atlantis model three letter code for functional group}
+#'\item{Name}{Atlantis model common name for functional group}
+#'\item{SciName}{scientific name of species}
+#'
+#' }
+#'
+#'
+"simFocalSpecies"
