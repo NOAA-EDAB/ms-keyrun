c6abde8972cf357436c6164bf2663579d0159c86
Sean Lucey <Sean.Lucey@NOAA.gov>
2021-09-22 14:54:17 -0400

    Seperated Albatross and Bigelow data sets

--- data-raw/R/pull_survdat.R
+++ data-raw/R/pull_survdat.R
@@ -1,14 +1,23 @@
 #Pull survey data - require you to be behind the NEFSC firewall
 library(here); library(data.table); library(survdat)
 
+#Refer to Data decisions Nov. 2020
+#Common temporal footprint of 1968 - 2019 for Spring and Fall survey
+#Separate data pulls for Albatross (A4) and Bigelow (HB)
+#Will eventually use SOE survey footprint
+
 #Connect to database
 channel <- dbutils::connect_to_database(server = 'sole', uid = 'slucey')
 
 #Pull data
-survdat <- survdat::get_survdat_data(channel)
+survdat.a4 <- survdat::get_survdat_data(channel, filterByYear = 1968:2008, 
+                                        conversion.factor = T)
+survdat.hb <- survdat::get_survdat_data(channel, filterByYear = 2009:2019,
+                                        conversion.factor = F)
 
 #save data
-save(survdat, file = here::here('data-raw', 'data', 'survdat.RData'))
+save(survdat.a4, file = here::here('data-raw', 'data', 'survdat_Albatross.RData'))
+save(survdat.hb, file = here::here('data-raw', 'data', 'survdat_Bigelow.RData'))
 
 #Pull bio data
 survdat.bio <- survdat::get_survdat_data(channel, getBio = T)
