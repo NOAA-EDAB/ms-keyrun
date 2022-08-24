e3279f95f33117a721cfd4513dd1e9b68747b35d
andybeet <22455149+andybeet@users.noreply.github.com>
2022-08-24 15:04:42 -0400

    added survey biomass and numbers by length summed over tows for a given year and season - realSurveyLennumcomp.rda (renamed) - renamed script to create this to realSurveyLennumcomp.r

---
+++ R/realSurveyLennumcomp.r
@@ -0,0 +1,23 @@
+#' Survey length composition for the mskeyrun project based on bottom trawl survey data
+#'
+#' Data pulled from the NEFSC bottom trawl survey database. Data pulled using 
+#'  \code{\link{survdat}} package.
+#' 
+#' @format A data frame
+#'
+#' \describe{
+#'\item{ModSim}{Atlantis model name and simulation id, or Actual data}
+#'\item{year}{year in which survey was undertaken}
+#'\item{season}{season in which survey was undertaken}
+#'\item{Code}{Species itis code}
+#'\item{Name}{Common name for species}
+#'\item{fishery}{fishery name}
+#'\item{lenbin}{recorded 1 cm length bin}
+#'\item{variable}{metric recorded, biomass & numbers}
+#'\item{value}{value of the variable, summed over all tows}
+#'\item{units}{units of the variable}
+#'
+#' }
+#'
+#'
+"realSurveyLennumcomp"
