af7c7d30b120022664e31a63513ef3593a739fea
andybeet <22455149+andybeet@users.noreply.github.com>
2022-09-20 12:57:36 -0400

    length samples by year and species for fishery

---
+++ R/fisheryLenSampN.r
@@ -0,0 +1,16 @@
+#' Fishery sampled number of lengths for input into hydra model
+#'
+#' Number of fish measured by year, species
+#' for mskeyrun focal species
+#'
+#' #' @format A data frame
+#' 
+#'  \describe{
+#'\item{YEAR}{year diet data collected}
+#'\item{SVSPP}{SVSPP code for species}
+#'\item{lensampsize}{number of fish measured for length}
+#'\item{SPECIES_ITIS}{SPECIES_ITIS code for species}
+#'\item{LongName}{Common name for species}
+#'
+#' }
+'fisheryLenSampN'
