837093840fcf22d7bb35b799844e9a198197cbe2
Sean Lucey <Sean.Lucey@NOAA.gov>
2022-09-23 15:26:26 -0400

    Added discards to data pull

--- data-raw/R/pull_comland.R
+++ data-raw/R/pull_comland.R
@@ -8,16 +8,25 @@ library(here); library(data.table); library(comlandr)
 channel <- dbutils::connect_to_database(server = 'sole', uid = 'slucey')
 
 #Pull data
-comland <- comlandr::get_comland_data(channel, filterByArea = c(521, 522, 525, 
-                                                                526, 537, 538, 
-                                                                551, 552, 561,
-                                                                562), 
+landings <- comlandr::get_comland_data(channel, filterByYear = 1968:2019,
+                                       filterByArea = c(521, 522, 525, 526, 537, 
+                                                        538, 551, 552, 561, 562), 
                                       aggArea = T, aggGear = T,
                                       unkVar = c('MONTH', 'Fleet', 'EPU'),
                                       knStrata = c('NESPP3', 'YEAR', 'EPU',
                                                    'HY', 'QY', 'MONTH', 
                                                    'Fleet', 'TONCL1'))
-comland$comland <- comland$comland[EPU == 'GB', ]
+
+#Pull discard data
+channel2 <- dbutils::connect_to_database('nova', 'slucey')
+
+discards <- comlandr::get_comdisc_data(channel2, landings, aggArea = T, aggGear = T,
+                                       extendTS = T)
+
+#Filter to just Georges Bank
+landings$comland <- landings$comland[EPU == 'GB', ]
+discards$comdisc <- discards$comdisc[EPU == 'GB', ]
 
 #save data
-save(comland, file = here::here('data-raw', 'data', 'comland.RData'))
+save(landings, file = here::here('data-raw', 'data', 'landings.RData'))
+save(discards, file = here::here('data-raw', 'data', 'discards.RData'))
