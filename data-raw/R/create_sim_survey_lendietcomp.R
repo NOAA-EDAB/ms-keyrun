fb1bd587b32478af1540d4fa86a4c905d6179435
Sarah Gaichas <sarah.gaichas@noaa.gov>
2023-06-15 17:37:15 -0400

    bugfix in sim surveylendiet

--- data-raw/R/create_sim_survey_lendietcomp.R
+++ data-raw/R/create_sim_survey_lendietcomp.R
@@ -13,9 +13,9 @@
 #'\item{Name}{Atlantis model common name for predator group}
 #'\item{survey}{simulated survey name}
 #'\item{agecl}{age class of Atlantis functional group}
-#'\item{pdlen}{predator length bin (1 cm)}
+#'\item{pdlencm}{predator length bin (1 cm)}
 #'\item{prey}{Atlantis model common name for prey group}
-#'\item{variable}{proportion in diet (dietprop)}
+#'\item{variable}{proportion of prey in diet at predator length (dietprop)}
 #'\item{value}{value of the variable}
 #'\item{units}{units of the variable}
 #'
@@ -24,80 +24,7 @@ library(magrittr)
 
 create_sim_survey_lendietcomp <- function(saveToData=T) {
 
-  # # input is path to model config file for atlantisom
-  # source(atlmod)
-  # 
-  # # path for survey and fishery config files
-  # cfgpath <- stringr::str_extract(atlmod, ".*config")
-  # 
-  # #works because atlantis directory named for model and simulation
-  # modpath <- stringr::str_split(d.name, "/", simplify = TRUE)
-  # modsim <- modpath[length(modpath)]
-  # 
-  # #read in survey annual age comp data
-  # all_diets <- atlantisom::read_savedsurvs(d.name, 'survDiet')
-  # 
-  # # get config files -- needed?
-  # svcon <- list.files(path=cfgpath, pattern = "*survey*", full.names = TRUE)
-  # 
-  # # read true list with run and biol pars, etc
-  # omlist_ss <- readRDS(file.path(d.name, paste0(scenario.name, "omlist_ss.rds")))
-  # 
-  # # model timesteps, etc from omdimensions script
-  # source(paste0(cfgpath,"/omdimensions.R"), local = TRUE)
-  # 
-  # #Number of years
-  # nyears <- omlist_ss$runpar$nyears
-  # total_sample <- omlist_ss$runpar$tstop/omlist_ss$runpar$outputstep
-  # 
-  # # user specified fit start and times if different from full run
-  # fitstartyr <- ifelse(!is.null(fitstart), fitstart-1, 0)
-  # fitendyr <- ifelse(!is.null(fitend), fitend, total_sample)
-  # 
-  # atlantis_full <- c(1:total_sample)
-  # mod_burnin <- fitstartyr*stepperyr+1
-  # fit_nyears <- fitendyr-fitstartyr
-  # fit_ntimes <- fit_nyears*stepperyr
-  # fittimes <- atlantis_full[mod_burnin:(mod_burnin+fit_ntimes-1)]
-  # #fit_timesteps <- seq(fittimes[stepperyr], max(fittimes), by=stepperyr) #last timestep
-  # #fit_years <- unique(floor(fittimes/stepperyr)) #from Christine's new sardine_config.R
-  # fittimes.days <- if(omlist_ss$runpar$outputstepunit=="days") fittimes*omlist_ss$runpar$outputstep
-  # 
-  # 
-  # # # survey cv lookup from config files
-  # # svcvlook <- tibble::tibble()
-  # # for(c in 1:length(svcon)){
-  # #   source(svcon[c], local = TRUE)
-  # #   surv_cv_n <- surv_cv %>%
-  # #     dplyr::mutate(survey=survey.name)
-  # #   svcvlook <- dplyr::bind_rows(svcvlook, surv_cv_n)
-  # # }
-  # 
-  # allsvdietprop <- tibble::tibble()
-  # 
-  # #multiple surveys named in list object
-  # for(s in names(all_diets)){
-  #   #arrange into wide format: year, Species1, Species2 ... and write csv
-  #   svdietprop <- all_diets[[s]][[1]] %>%
-  #     dplyr::filter(time.days %in% fittimes.days) %>%
-  #     dplyr::mutate(year = ceiling(time.days/365)) %>%
-  #     dplyr::select(species, agecl, year, prey, dietprop=dietSamp) %>%
-  #     dplyr::left_join(dplyr::select(omlist_ss$funct.group_ss, Code, Name), by = c("species" = "Name")) %>%
-  #     dplyr::mutate(ModSim = modsim) %>%
-  #     dplyr::mutate(survey = s) %>%
-  #     #dplyr::left_join(svcvlook) %>%
-  #     dplyr::select(ModSim, year, Code, Name=species, survey, agecl, everything()) %>%
-  #     tidyr::pivot_longer(cols = c("dietprop"),
-  #                         names_to = "variable",
-  #                         values_to = "value") %>%
-  #     dplyr::mutate(units = ifelse(variable=="dietprop", "proportion", "NA")) %>%
-  #     dplyr::arrange(Name, survey, variable, year, agecl)
-  # 
-  #   allsvdietprop <- dplyr::bind_rows(allsvdietprop, svdietprop)
-  # }
-  # 
-  # simSurveyDietcomp <- allsvdietprop
-  
+  #uses existing mskeyrun datasets, ensure they are up to date
   survagelen <- mskeyrun::simSurveyAgeLencomp
   survdiet <- mskeyrun::simSurveyDietcomp
   
@@ -110,7 +37,7 @@ create_sim_survey_lendietcomp <- function(saveToData=T) {
     #dplyr::filter(modbin.min <= lenbin & lenbin < modbin.max) %>% #lenbin defined as lower
     dplyr::group_by(ModSim, Code, Name, survey, year, agecl, lenbin) %>%
     dplyr::summarise(sumlen = sum(value)) %>%
-    dplyr::group_by(Name, year, lenbin) %>%
+    dplyr::group_by(Name, survey, year, lenbin) %>%
     dplyr::mutate(propage = sumlen/sum(sumlen)) #proportion of each agecl contributing to lengthbin
   
   simSurveyLenDietcomp <- survdiet %>%
@@ -125,9 +52,8 @@ create_sim_survey_lendietcomp <- function(saveToData=T) {
     dplyr::ungroup() %>%
     #dplyr::filter(!is.na(sizebin)) 
     dplyr::mutate(variable = "dietprop",
-                  value = dietsize,
                   units = "proportion") %>%
-    dplyr::select(-dietsize)
+    dplyr::rename(value = dietsize, pdlencm = lenbin)
   
   
   
