72e1b4cc2d604e0352d2c0e54aacf7e6beafcbbc
Sarah Gaichas <sarah.gaichas@noaa.gov>
2023-10-03 16:03:52 -0400

    has all age structured groups not yet biomass pools

---
+++ R/simSurveyIndexFW.r
@@ -0,0 +1,21 @@
+#' The Atlantis-simulated survey biomass indices for the mskeyrun project
+#'
+#' Simulated survey index data from Atlantis model as sampled by atlantisom
+#' Includes all structured groups as separate surveys
+#'
+#' @format A data frame
+#'
+#' \describe{
+#'\item{ModSim}{Atlantis model name and simulation id}
+#'\item{year}{year simulated survey conducted}
+#'\item{Code}{Atlantis model three letter code for functional group}
+#'\item{Name}{Atlantis model common name for functional group}
+#'\item{survey}{simulated survey name}
+#'\item{variable}{biomass or coefficient of variation (cv) of biomass}
+#'\item{value}{value of the variable}
+#'\item{units}{units of the variable}
+#'
+#' }
+#'
+#'
+"simSurveyIndexFW"
